e59e3506a269ff10768ed194137fa1cf02e5759a
iampr <iampr@PRABIN>
2023-01-02 23:28:04 +0545

    Add project files.

---
+++ asabeginner2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter your FirstName");
+        string FiratName = Console.ReadLine();
+        Console.WriteLine("Enter your LastName");
+        string LastName = Console.ReadLine();
+        //Console.WriteLine("Hello  " +  UserName);
+        Console.WriteLine("hello {0} {1}", FiratName , LastName);
+    }
+}

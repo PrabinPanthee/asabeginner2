519540e588979a7f8e699141b6a8996fcb7e9809
PrabinPanthee <121803999+PrabinPanthee@users.noreply.github.com>
2023-01-02 23:30:59 +0545

    Update Program.cs

--- asabeginner2/Program.cs
+++ asabeginner2/Program.cs
@@ -4,10 +4,10 @@ class Program
     static void Main()
     {
         Console.WriteLine("Enter your FirstName");
-        string FiratName = Console.ReadLine();
+        string FirstName = Console.ReadLine();
         Console.WriteLine("Enter your LastName");
         string LastName = Console.ReadLine();
         //Console.WriteLine("Hello  " +  UserName);
-        Console.WriteLine("hello {0} {1}", FiratName , LastName);
+        Console.WriteLine("hello {0} {1}", FirstName , LastName);
     }
 }
